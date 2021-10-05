66c1712b7ae80d38d55d3ef0c059fbabd34cbaf8
root <root@ip-172-31-9-81.ap-south-1.compute.internal>
2021-10-05 12:46:44 +0000

    First commit

---
+++ terraform/webapp/main_webapp.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+    region = "ap-south-1"
+    profile = var.profile
+}
+
+resource "aws_instance" "webapp_node" {
+    ami = "ami-0fadaffaadf470b9c"
+    instance_type = "t2.micro"
+    key_name = "rabbitmq"
+    vpc_security_group_ids = ["sg-0d69a254aedef9bd5"]
+
+    tags = {
+        Name = var.name
+        group = var.group
+    }
+}
